5e2fb51fbe65c962cad26237cea220c54eb637cf
Akshay <akshayrambady11@google.com>
2026-03-28 02:00:53 +0000

    March 28: wrote shell script for [Node Health]

---
+++ greptest.sh
@@ -0,0 +1,5 @@
+echo 1
+echo 12
+echo 16
+echo 45
+echo 56
---
+++ node_health.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+###############################
+# Author : Akshay.
+# Date : 27-03-2026
+#
+# This script outputs the node health. 
+#
+# V1
+################################
+
+
+
+set -x # debug mode 
+
+df -h
+
+
+free -g
+
+
+nproc
+
+
