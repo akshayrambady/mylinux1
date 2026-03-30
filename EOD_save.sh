d5f4ce631c69a93956a79b7e1e35e83bf3e12bff
Akshay <akshayrambady11@google.com>
2026-03-30 17:52:45 +0000

    30-March Automated git push

--- EOD_save.sh
+++ EOD_save.sh
@@ -3,15 +3,16 @@
 #####
 #This code is to push my edits into git rep by EOD.
 #Author: Akshay.
-#Version: v0.0.2
+#Version: v1.0
 #Last edited on 30-March
 ###########
 #
 set x
 git add .
 echo "Enter what do did today."
-read data
-git commit -m "$data"
+read EOD_message
+git commit -m "$EOD_message"
+git pull origin main --rebase
+git push origin main
 
-echo success
-#git push origin main
+echo "Succesfully pushed your today's work into git repository"
