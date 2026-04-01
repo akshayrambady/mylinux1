06f4a61d37e81327c115ad7b9b29b4412f7a75c7
Akshay <akshayrambady11@google.com>
2026-04-01 10:39:03 +0000

    01-April - Important linux commands.

---
+++ top_10_shellcommands.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+###################################
+#10 important Linux commands for DevOps.
+#Author : Akshay.
+#version: v0.01
+#
+##################################
+#
+set -ex
+top
+htop
+ps
+ps aux
+pstree -p
+pstree unknwnprocess
+netstat tuln
+tcpdump
+sudo tcpdump -i eth0
