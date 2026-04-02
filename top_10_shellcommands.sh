36b629fb6cecd06a4035474452765f81b2d74a31
Akshay <akshayrambady11@google.com>
2026-04-02 10:16:56 +0000

    02-April - Modified top 10 linux

--- top_10_shellcommands.sh
+++ top_10_shellcommands.sh
@@ -3,7 +3,7 @@
 ###################################
 #10 important Linux commands for DevOps.
 #Author : Akshay.
-#version: v0.01
+#version: v0.0.2
 #
 ##################################
 #
@@ -17,3 +17,17 @@ pstree unknwnprocess
 netstat tuln
 tcpdump
 sudo tcpdump -i eth0
+ping something.com
+traceroute something.com
+
+df -h
+du -sh filename
+free -h
+
+journalctl -u servicename
+journalctl -b
+
+lsof -i :53
+
+tail ? head ? cat /var/log/logfilename
+
